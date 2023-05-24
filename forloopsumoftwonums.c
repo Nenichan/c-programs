82983f4c7dfcc28de4beb681ba06cdfcbd993da9
Nenichan <133579689+Nenichan@users.noreply.github.com>
2023-05-24 14:06:32 +0530

    forloopsumoftwonums.c

--- forloopsumoftwonums.c
+++ forloopsumoftwonums.c
@@ -4,9 +4,9 @@ void main()
     int i,n,p=0;
     printf("enter any number:");
     scanf("%d",&n);
-    for(i=1; i<=10; i++)
+    for(i=1; i<=n; i++)
     {
-        p=n+i;
+        p=p+i;
         printf("the sum of %d is %d\n",n,p);
     }
 }
